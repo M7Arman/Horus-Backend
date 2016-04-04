816bd4466fee0f75fa7825664c5fc99fd1f27f17
Arman Kostandyan <armankostandyan@gmail.com>
2016-04-04 12:42:22 +0400

    - Added functionality to show clicked item detail

--- app/src/main/java/com/arman/horus/activities/TripDetailActivity.java
+++ app/src/main/java/com/arman/horus/activities/TripDetailActivity.java
@@ -3,6 +3,7 @@ package com.arman.horus.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.arman.horus.R;
 import com.arman.horus.models.TripDetail;
@@ -26,8 +27,12 @@ public class TripDetailActivity extends AppCompatActivity {
 
     private void showTripDetail(String id) {
         TripDetail tripDetail = TripDetail.dummyTripDetail();
-        // TripDetailAdapter adapter = new TripDetailAdapter(tripDetail);
-        // adapter.bind();
+        getSupportActionBar().setTitle(tripDetail.title);
+        ImageView imageView = (ImageView) findViewById(R.id.item_detail_image);
+        imageView.setImageResource(tripDetail.images);
+//        TextView descriptionView = (TextView) findViewById(R.id.item_detail_description);
+//        descriptionView.setText(tripDetail.description);
+//        TripDetailAdapter.adapt(tripDetail);
         // TODO: create trip detail adapter
         // TODO: pass the trip detail data to adapter to show view
     }
